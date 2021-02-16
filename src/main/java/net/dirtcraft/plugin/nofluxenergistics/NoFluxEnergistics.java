f80cd54f54d57236a33cb459eaa15c70b160b9b8
d4julian <47072615+d4julian@users.noreply.github.com>
2021-02-16 01:49:39 -0500

    add a few items for SF4

--- src/main/java/net/dirtcraft/plugin/nofluxenergistics/NoFluxEnergistics.java
+++ src/main/java/net/dirtcraft/plugin/nofluxenergistics/NoFluxEnergistics.java
@@ -11,6 +11,7 @@ import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageReceiver;
 import org.spongepowered.api.text.serializer.TextSerializers;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
@@ -35,6 +36,16 @@ public class NoFluxEnergistics {
     private static final String YABBA = "Yabba";
     private static final String STORAGE_DRAWERS = "StorageDrawers";
 
+    private static final String BONSAI_POT = "bonsaitrees:bonsaipot";
+    private static final String ITEM_DUCTS = "thermaldynamics:duct_32";
+
+    private static final String MINECRAFT_HOPPER = "minecraft:hopper";
+    private static final String COBBLE_GEN = "tp:cobblegen_block";
+    private static final String COBBLE_GEN_2 = "tp:iron_cobblegen_block";
+    private static final String COBBLE_GEN_3 = "tp:diamond_cobblegen_block";
+    private static final String COBBLE_GEN_4 = "tp:blaze_cobblegen_block";
+    private static final String COBBLE_GEN_5 = "tp:emerald_cobblegen_block";
+
     private static final String BLACK_HOLE = "industrialforegoing:black_hole";
 
     @Inject
@@ -46,21 +57,32 @@ public class NoFluxEnergistics {
         Player player = (Player) cause;
 
         for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
-            if (isNextTo(transaction, AE2, FLUX_NETWORKS)) notifyPlayer(event, player, AE2, FLUX_NETWORKS);
-            if (isNextTo(transaction, AE2, PROJECT_E)) notifyPlayer(event, player, AE2, PROJECT_E);
-            if (isNextTo(transaction, AE2, YABBA)) notifyPlayer(event, player, AE2, YABBA);
-            if (isNextTo(transaction, AE2, STORAGE_DRAWERS)) notifyPlayer(event, player, AE2, STORAGE_DRAWERS);
-
-            if (isNextTo(transaction, AE2, BLACK_HOLE)) notifyPlayer(event, player,
-                    "&7You &ccannot &7place items from &6Applied Energistics 2 &7next to &6Black Hole Storage&7 items" +
-                            "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!\"");
+            BlockSnapshot blockSnapshot = transaction.getFinal();
+            checkBlocks(blockSnapshot, event, player);
         }
     }
 
-    private boolean isNextTo(Transaction<BlockSnapshot> transaction, String... itemIds) {
+    public void checkBlocks(BlockSnapshot blockSnapshot, ChangeBlockEvent.Place event, MessageReceiver source) {
+        if (isNextTo(blockSnapshot, AE2, FLUX_NETWORKS)) notifyPlayer(event, source, AE2, FLUX_NETWORKS);
+        if (isNextTo(blockSnapshot, AE2, PROJECT_E)) notifyPlayer(event, source, AE2, PROJECT_E);
+        if (isNextTo(blockSnapshot, AE2, YABBA)) notifyPlayer(event, source, AE2, YABBA);
+        if (isNextTo(blockSnapshot, AE2, STORAGE_DRAWERS)) notifyPlayer(event, source, AE2, STORAGE_DRAWERS);
+
+        if (isNextTo(blockSnapshot, AE2, BLACK_HOLE)) notifyPlayer(event, source,
+                "&7You &ccannot &7place items from &6Applied Energistics 2 &7next to &6Black Hole Storage&7 items" +
+                        "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!");
+        if (isNextTo(blockSnapshot, BONSAI_POT, ITEM_DUCTS)) notifyPlayer(event, source,
+                "&7You &ccannot &7place &6Bonsai Pots&7 next to &6ItemDucts" +
+                        "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!");
+        if (isNextTo(blockSnapshot, MINECRAFT_HOPPER, COBBLE_GEN, COBBLE_GEN_2, COBBLE_GEN_3, COBBLE_GEN_4, COBBLE_GEN_5)) notifyPlayer(event, source,
+                "&7You &ccannot &7place &6Hoppers&7 next to &6Cobblestone Generators" +
+                        "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!");
+    }
+
+    private boolean isNextTo(BlockSnapshot blockSnapshot, String... itemIds) {
         List<String> ids = Arrays.stream(itemIds).map(String::toLowerCase).collect(Collectors.toList());
 
-        BlockState block = transaction.getFinal().getExtendedState();
+        BlockState block = blockSnapshot.getExtendedState();
         String blockId = block.getId().toLowerCase();
 
         boolean match = false;
@@ -71,9 +93,9 @@ public class NoFluxEnergistics {
             }
         if (!match) return false;
 
-        Optional<Location<World>> optionalLocation = transaction.getFinal().getLocation();
+        Optional<Location<World>> optionalLocation = blockSnapshot.getLocation();
         if (!optionalLocation.isPresent()) {
-            logger.error("Could not retrieve block location for block transaction: " + transaction.toContainer());
+            logger.error("Could not retrieve block location for block transaction: " + blockSnapshot.toContainer());
             return false;
         }
         Location<World> blockLocation = optionalLocation.get();
@@ -88,25 +110,25 @@ public class NoFluxEnergistics {
 
         for (Location<World> location : locations) {
             String locationId = location.getBlock().getId().toLowerCase();
-            for (String id : ids) if (!locationId.startsWith(blockId.split(":")[0]) && locationId.startsWith(id)) return true;
+            for (String id : ids) if (!blockId.toLowerCase().startsWith(locationId.toLowerCase()) && locationId.startsWith(id)) return true;
         }
 
         return false;
     }
 
-    private void notifyPlayer(ChangeBlockEvent.Place event, Player player, String id1, String id2) {
+    private void notifyPlayer(ChangeBlockEvent.Place event, MessageReceiver source, String id1, String id2) {
         event.setCancelled(true);
         id1 = splitCamelCase(id1);
         id2 = splitCamelCase(id2);
-        player.sendMessage(format("&7You &ccannot &7place items from &6" + id1 + " &7next to items from &6" + id2 + "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!"));
+        source.sendMessage(format("&7You &ccannot &7place items from &6" + id1 + " &7next to items from &6" + id2 + "\n&7This is to prevent severe lag and/or server crashes so please use an alternative!"));
     }
 
-    private void notifyPlayer(ChangeBlockEvent.Place event, Player player, String message) {
+    private void notifyPlayer(ChangeBlockEvent.Place event, MessageReceiver source, String message) {
         event.setCancelled(true);
-        player.sendMessage(format(message));
+        source.sendMessage(format(message));
     }
 
-    private static Text format(String message) {
+    public static Text format(String message) {
         return TextSerializers.FORMATTING_CODE.deserialize(message);
     }
 
